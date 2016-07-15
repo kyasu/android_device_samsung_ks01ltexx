87628b049ab28387fcc7c1151f15f658cfeb2745
kyasu <kyasu.dev@gmail.com>
2016-07-16 01:18:17 +0900

    ks01lte: Migrate selective-spn from common repo to device tree
    As of CM13, selective-spn use was moved to device tree.
    See: http://review.cyanogenmod.org/114872
    
    Removal from /vendor/cm  http://review.cyanogenmod.org/133418

--- device.mk
+++ device.mk
@@ -128,6 +128,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# SPN override
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
