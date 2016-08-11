b64cda1965cda34fa15b49d73fca3a1dfcfd5287
kyasu <kyasu.dev@gmail.com>
2016-08-11 14:14:09 +0900

    ks01lte: Use LZ4 for memory compression

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ USE_CLANG_PLATFORM_BUILD := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 zcache.enabled=1
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 zcache.enabled=1 zcache.compressor=lz4
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
