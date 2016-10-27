58642dd44ed75de99ce840713c76e3318d6a5cd6
kyasu <kyasu.dev@gmail.com>
2016-10-28 08:38:56 +0900

    ks01ltexx: Set PRODUCT_AAPT_PREBUILT_DPI.
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.

--- device.mk
+++ device.mk
@@ -26,8 +26,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 -include $(LOCAL_PATH)/system_prop.mk
 
 # Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi mdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
