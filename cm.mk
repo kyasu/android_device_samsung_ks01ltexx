782d256873c2147670ab6cada9e5c76d9a7a7e77
kyasu <kyasu.dev@gmail.com>
2016-11-19 10:04:53 +0900

    ks01ltexx: Remove enhanced NFC feature
    This is no longer used anywhere.

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/ks01ltexx/full_ks01ltexx.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
