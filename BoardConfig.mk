31ed18b92b3e0a5ebf6338db5428a1003dcdbd1d
kyasu <kyasu.dev@gmail.com>
2017-09-13 05:58:47 +0900

    ks01ltexx: Use full defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,9 +40,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-TARGET_KERNEL_CONFIG := cm_msm8974_sec_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := cm_msm8974_sec_ks01eur_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := cm_selinux_defconfig
+TARGET_KERNEL_CONFIG := lineage_ks01lte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8974
 
 
