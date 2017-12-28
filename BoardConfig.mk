80c17ecc42f2f37ce5d920779ed435fa2420ca0e
kyasu <kyasu.dev@gmail.com>
2017-12-28 23:12:44 +0900

    ks01ltexx: Add AID_QCOM_DIAG, AID_RFS, and AID_RFS_SHARED to config.fs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Extended Filesystem Support
 TARGET_KERNEL_HAVE_EXFAT := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := device/samsung/klte-common/config.fs
+
 # Legacy BLOB Support
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
