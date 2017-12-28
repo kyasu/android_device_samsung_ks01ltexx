f7b40730663b4373116a21d8c5f8a7df4788a3e5
kyasu <kyasu.dev@gmail.com>
2017-12-28 17:26:43 +0900

    ks01ltexx: Add empty HIDL manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,9 @@ TARGET_KERNEL_HAVE_EXFAT := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := device/samsung/klte-common/config.fs
 
+# HIDL
+DEVICE_MANIFEST_FILE += device/samsung/ks01ltexx/manifest.xml
+
 # Legacy BLOB Support
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
