d4e59e9ef052a9a9d46fd7b41d21bc46959db1b3
kyasu <kyasu.dev@gmail.com>
2018-01-13 11:52:09 +0900

    ks01ltexx: Update the OSS libril for O
    * Kang from klte-common

--- libril/Android.mk
+++ libril/Android.mk
@@ -5,25 +5,35 @@ ifeq ($(BOARD_PROVIDES_LIBRIL),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_VENDOR_MODULE := true
+
 LOCAL_SRC_FILES:= \
     ril.cpp \
     ril_event.cpp\
-    RilSocket.cpp \
     RilSapSocket.cpp \
+    ril_service.cpp \
+    sap_service.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
-    libbinder \
     libcutils \
     libhardware_legacy \
     librilutils \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio.deprecated@1.0 \
+    libhidlbase  \
+    libhidltransport \
+    libhwbinder
 
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
 
+LOCAL_CFLAGS += -Wall -Wextra -Wno-unused-parameter -Werror
+
 ifeq ($(SIM_COUNT), 2)
-    LOCAL_CFLAGS := -DANDROID_MULTI_SIM
+    LOCAL_CFLAGS += -DANDROID_MULTI_SIM -DDSDA_RILD1
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
@@ -32,34 +42,9 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
 
 LOCAL_MODULE:= libril
-ifeq ($(TARGET_NEEDS_GCC_LIBRIL),true)
-    LOCAL_CLANG := false
-else
-    LOCAL_CLANG := true
-    LOCAL_SANITIZE := integer
-endif
+LOCAL_CLANG := true
+LOCAL_SANITIZE := integer
 
 include $(BUILD_SHARED_LIBRARY)
 
-
-# For RdoServD which needs a static library
-# =========================================
-ifneq ($(ANDROID_BIONIC_TRANSITION),)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-    ril.cpp
-
-LOCAL_STATIC_LIBRARIES := \
-    libutils_static \
-    libcutils \
-    librilutils_static \
-    libprotobuf-c-nano-enable_malloc
-
-LOCAL_CFLAGS :=
-
-LOCAL_MODULE:= libril_static
-
-include $(BUILD_STATIC_LIBRARY)
-endif # ANDROID_BIONIC_TRANSITION
 endif # BOARD_PROVIDES_LIBRIL
