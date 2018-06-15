a23f6fb35ba48ffc9588822b4637b25f25b11da6
kyasu <kyasu.dev@gmail.com>
2018-06-15 12:23:22 +0900

    ks01ltexx: Rework shims

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,10 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Legacy BLOB Support
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-TARGET_LD_SHIM_LIBS := /system/vendor/lib/hw/camera.vendor.msm8974.so|libshim_camera.so:/system/lib/libcutils.so|libshim_cutils_atomic.so
+TARGET_LD_SHIM_LIBS += \
+    /system/vendor/lib/hw/camera.vendor.msm8974.so|libshim_camera.so \
+    /system/vendor/lib/libsec-ril.so|libshim_cutils_atomic.so \
+    /system/vendor/bin/mpdecision|libshim_cutils_atomic.so
 
 # LineageHW
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
