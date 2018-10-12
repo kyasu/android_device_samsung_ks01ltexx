beef219618439868254535040babeafd2c077521
kyasu <kyasu.dev@gmail.com>
2018-10-13 07:48:04 +0900

    Revert "ks01ltexx: nfc: Import NFC HAL from msm8930-common"
    This reverts commit a1535aaf561a5fbe06491c10a00330aa506dbda4.
    
    * Use LineageOS Broadcom NFC HAL recently imported.

--- device.mk
+++ device.mk
@@ -95,9 +95,9 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-impl-bcm \
     android.hardware.nfc@1.0-service \
-    nfc_nci.msm8974 \
+    nfc_nci.bcm2079x.default \
     com.android.nfc_extras \
     NfcNci \
     Tag
