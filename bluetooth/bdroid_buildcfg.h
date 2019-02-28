27864fe145f7ce1ae65c6cfaaae063c01b884ba1
kyasu <kyasu.dev@gmail.com>
2019-02-28 15:17:46 +0900

    ks01ltexx: Set default hotspot name
    * Make default bluetooth name same

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S4 LTE-A"
+#define BTM_DEF_LOCAL_NAME   "Galaxy S4 LTE-A"
 
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
