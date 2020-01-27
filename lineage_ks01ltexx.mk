7f2510b176a9ba709f976a36b61d1fafecbafc11
kyasu <kyasu.dev@gmail.com>
2020-01-27 22:29:23 +0900

    ks01ltexx: Remove overridden apns-conf.xml

--- lineage_ks01ltexx.mk
+++ lineage_ks01ltexx.mk
@@ -15,9 +15,6 @@
 # limitations under the License.
 #
 
-PRODUCT_COPY_FILES += \
-    device/samsung/ks01ltexx/configs/apns-conf.xml:system/etc/apns-conf.xml
-
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
